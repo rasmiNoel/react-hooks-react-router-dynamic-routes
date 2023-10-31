8a9bfe1082acd4989c73c379393c94a848023c5b
rasmi <rasminoel@gmail.com>
2023-10-31 21:10:11 +0300

    Done With Assignment

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+      {/* Adding code to show a message to the user to select a movie if they haven't yet */}
+      <Route exact path={match.url}>
+        <h3>Please select a Movie from the list above</h3>
+      </Route>
+      {/* We also add a `Route` component that will render `MovieShow`
+      when a movie is selected */}
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
+
   );
 }
 export default MoviesPage;
